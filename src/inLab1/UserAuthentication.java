c2fc19589070c11df5591603af1357b49ff0d887
Manahil Malik <f24608055@nutech.edu.pk>
2026-05-17 21:29:41 +0500

    Resolved merge conflict in login()

--- src/inLab1/UserAuthentication.java
+++ src/inLab1/UserAuthentication.java
@@ -1,8 +1,8 @@
 package inLab1;
 
 public class UserAuthentication {
-    public void login() {
-        System.out.println("Login successful - Manahil");
-        System.out.println("Login successful - Student B");
-    }
+	public void login() {
+		System.out.println("Login successful - Manahil ");
+		System.out.println("Login successful - Hammad ");
+	}
 }
